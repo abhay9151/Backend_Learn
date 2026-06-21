a02380e8e57bb6030f9e60cbf0c000d77a2e92e4
Abhay Pratap Singh <saurabhsingh99195@gmail.com>
2026-06-22 00:38:17 +0530

    Express server making

--- Day06/index.js
+++ Day06/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 
-// 
+// create a server using express
 const app = express();
 
 // "/detail"
@@ -14,7 +14,16 @@ const app = express();
 // * any number of character can arrive
 //  rohit_negi9
 
-app.use("/about/:id", (req,res)=>{
+// app.use is a method in Express.js 
+// that is used to define middleware functions that will be executed for every incoming request to the server. 
+// It allows you to specify a path and a callback function that will be executed when a request matches that path.
+//  The callback function takes two parameters: req (the request object) and res (the response object).
+//  You can use app.use to handle different routes and send responses back to the client based on the requested URL.
+
+
+//we are using Route Parameters (:id and :user) to capture dynamic values from the URL.
+
+app.use("/about/:id/:user", (req,res)=>{
         console.log(req.params);
         res.send({"name":"Rohit", "age":20, "money":70, "Mon":20});
 })
@@ -27,7 +36,7 @@ app.use("/about/:id", (req,res)=>{
 
 
 // app.use("/about", (req,res)=>{
-//     res.send({"name":"Rohit", "age":20, "money":70, "Mon":20});
+//     res.send({"name":"Rohit", "age":20, "money":70, "Mon":3000});
 // })
 
 // app.use("/contact", (req,res)=>{
@@ -38,13 +47,14 @@ app.use("/about/:id", (req,res)=>{
 //     res.send("I am your Detail Page");
 // })
 
+// // In Express, route order matters. Specific routes should be defined before general routes like app.use("/") because Express processes routes from top to bottom.
 // app.use("/", (req,res)=>{
 //     res.send("I am Your Home Page");
 // })
 
 
 
-
+// yaha se ye listen karega ki server chal raha hai ya nahi.
 app.listen(4000, ()=>{
     console.log("Listening at port 4000");
 })
