9430678956168901a6f1862f686887efdaa904d7
Abhay Pratap Singh <saurabhsingh99195@gmail.com>
2026-07-14 23:19:52 +0530

    comment

--- MongooseValiditation/database.js
+++ MongooseValiditation/database.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 async function main() {
    
   await mongoose.connect("mongodb+srv://coderArmy9:Hunter%409Bhai@codingadda.4ugikcf.mongodb.net/Instagram");
+  //database name is Instagram, if it is not present then it will create a new database with the name Instagram
   
 }
 
